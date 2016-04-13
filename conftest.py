b0b653cb288f9f6e9ac02a9c659b2735fc865503
vankrajnova <a-kraynova@marya.ru>
2016-04-13 12:13:21 +0300

    Создан config_file

--- conftest.py
+++ conftest.py
@@ -1,26 +1,29 @@
+import json
 import pytest
 from fixture.application import Application
 
+
 fixture = None
+target = None
 
 
 @pytest.fixture(scope="session", autouse=True)
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default = "firefox")
-    parser.addoption("--baseUrl", action="store", default="http://bx.marya.ru/")
+    parser.addoption("--target", action="store", default="target.json")
 
 
 
